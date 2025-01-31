3be0d0832eba2647cee9a5f4b3e59ea14fc50f18
Dawid Pruski(Prss161) <dpruski0@gmail.com>
2025-01-31 23:01:48 +0100

    Refactor ad handling logic and extract find_and_click function to navigation module

---
+++ lib/navigation.py
@@ -0,0 +1,50 @@
+import time
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+def find_and_click(driver, timeout=10):
+    end_time = time.time() + timeout
+    ad_cases = get_ad_cases()
+    while time.time() < end_time:
+        for by, locators in ad_cases.items():
+            for locator in locators:
+                try:
+                    print(f"Trying to find element: [{by}] {locator}")
+                    if by == "xpath":
+                        element = driver.find_element(AppiumBy.XPATH, locator)
+                    elif by == "id":
+                        element = driver.find_element(
+                            AppiumBy.ACCESSIBILITY_ID, locator
+                        )
+                    if element.is_displayed():
+                        element.click()
+                        print(f"Clicked element with locator: [{by}] {locator}")
+                        return True
+                except:
+                    time.sleep(0.5)
+
+    print(f"Failed to find and click any element: [{by}] {locator}")
+    return False
+
+
+def get_ad_cases():
+    return {
+        "xpath": [
+            '//android.widget.ImageButton[@content-desc="Close"]',
+            '//android.view.View[@resource-id="skipPlayableButton"]/android.view.View/android.view.View/android.widget.Image',
+            '//android.view.View[@resource-id="closeButton"]/android.view.View/android.view.View/android.widget.Image',
+            '//android.view.View[@resource-id="skipButton"]/android.widget.TextView',
+            '//android.view.View[@resource-id="storePromoContainer"]/android.view.View/android.widget.Image[1]',
+            '//android.widget.TextView[@text=""]',
+            '//android.widget.Button[@text="Close"]',
+            "//android.widget.Button",
+            '//android.view.View[@resource-id="end-screen-adapter"]/android.view.View[1]/android.widget.Image',
+            '//android.widget.Image[@text="close"]',
+        ],
+        "id": [
+            "closeButton",
+            "com.rapidfiregames.backpackbrawl:id/ia_iv_close_button",
+            "Close",
+        ],
+        # "tag name": ["button"],
+    }
--- main.py
+++ main.py
@@ -1,71 +1,62 @@
 from appium.webdriver.appium_service import AppiumService
 from appium import webdriver
-from selenium.webdriver.common.by import By
 from appium.options.android import UiAutomator2Options
+from lib.navigation import find_and_click
 import time
 
 appium_service = AppiumService()
+
 options = UiAutomator2Options()
 options.platform_name = "Android"
 options.automation_name = "UiAutomator2"
-options.device_name = "127.0.0.1:5555"  # Replace with your emulator/device ID
+options.device_name = "127.0.0.1:5555"
+
 
 def main():
     try:
         appium_service.start()
         print("Appium server started!")
-
         launch_app(10)
     except Exception as e:
-        print(f"Error Message: {e}")        
+        print(f"Error Message: {e}")
     finally:
         appium_service.stop()
-        
-def find_and_click(driver, by: By, locator: str, timeout=10):
-    end_time = time.time() + timeout
-    while time.time() < end_time:
-        try:
-            element = driver.find_element(by, locator)
-            if element.is_displayed():
-                element.click()
-                print(f"Clicked element with locator: {locator}")
-                return True
-        except:
-            time.sleep(0.5)
-    print(f"Element not found: {locator}")
-    return False
+
 
 def launch_app(ads: int):
-    driver = webdriver.Remote('http://localhost:4723', options=options)
-    
-    # driver.tap([(165, 1530)])
-    # driver.swipe(450, 1450, 450, 300, 500)
-    # time.sleep(5)
     ad = 0
+    driver = webdriver.Remote("http://localhost:4723", options=options)
     repeat = False
+    window_size = driver.get_window_size()
+    window_x = window_size["width"]
+    window_y = window_size["height"]
+    print(f"{window_x}, {window_y}")
+    driver.tap([(window_x / 5, window_y / 1.01)])
+    driver.swipe(window_x / 2, window_y / 1.1, window_x / 2, window_y / 3, 550)
+    time.sleep(5)
     while ad < ads:
-        if repeat == False:
-            driver.tap([(450, 260)])
-            time.sleep(45)
-        find_and_click(driver, By.XPATH, '//android.widget.ImageButton[@content-desc="Close"]')
-        find_and_click(driver, By.XPATH, '//android.view.View[@resource-id="skipPlayableButton"]/android.view.View/android.view.View/android.widget.Image')
-        find_and_click(driver, By.XPATH, '//android.view.View[@resource-id="closeButton"]/android.view.View/android.view.View/android.widget.Image')
-        find_and_click(driver, By.XPATH, '//android.view.View[@resource-id="skipButton"]/android.widget.TextView')
-        find_and_click(driver, By.XPATH, '//android.view.View[@resource-id="storePromoContainer"]/android.view.View/android.widget.Image[1]')
-        find_and_click(driver, By.XPATH, '//android.widget.ImageView[@resource-id="com.rapidfiregames.backpackbrawl:id/ia_iv_close_button"]')
-        find_and_click(driver, By.XPATH, '//android.widget.TextView[@text=""]')
-        find_and_click(driver, By.XPATH, '//android.widget.Button[@text="Close"]')
-        find_and_click(driver, By.TAG_NAME, 'button')
-        find_and_click(driver, By.CLASS_NAME, 'android.widget.Button')
-        game_view = driver.find_element(By.XPATH, '//android.view.View[@content-desc="Game view"]')
-        if game_view.is_displayed():
-            ad += 1
-            repeat = False
-            print("Waiting for next ad")
-            time.sleep(1805)
-        else:
-            print("Didn't find any element to close ad, repeating process")
+        try:
+            if repeat == False:
+                driver = webdriver.Remote("http://localhost:4723", options=options)
+                driver.tap([(window_x / 2, window_y / 2)])
+                time.sleep(30)
+            find_and_click(driver)
+            game_view = driver.find_element(
+                "xpath", '//android.view.View[@content-desc="Game view"]'
+            )
+            if game_view.is_displayed():
+                ad += 1
+                repeat = False
+                print(f"Ad {ad}/{ads} completed. Waiting for the next ad...")
+                driver.quit()
+                time.sleep(1800)
+            else:
+                print("Game view not visible. Repeating process...")
+                repeat = True
+
+        except Exception as e:
+            print(f"Error during ad handling: {e}")
             repeat = True
-            continue
+
 
 main()
